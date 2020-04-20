fc0fabfb49ef646ee7a4a350007d3da1b449a2fd
rendrap <rendrapradana@gmail.com>
2020-04-20 19:32:21 +0700

    add todo_items controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  resources :todo_lists
+
+  resources :todo_lists do
+    resources :todo_items
+  end
+
+
+  root "todo#index"
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
